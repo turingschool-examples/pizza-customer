4a71fec3e385011b3a12d02131eebd0922c95732
Seth <seth19483@gmail.com>
2017-02-24 14:02:19 -0700

    done

---
+++ lib/customer.rb
@@ -0,0 +1,45 @@
+class Customer
+	attr_reader :name, :address, :phone
+
+	def initialize(name, address, phone_number)
+		@name = name
+		@address = address
+		@phone = phone_number
+		@order = false
+		@frequency = []
+		@count = 0
+	end
+
+	def profile
+		{ name: name, address: address, phone: phone }
+	end
+
+	def order_in_place?
+		@order
+	end
+
+	def place_order(pizza, time)
+		@order = true
+		@frequency << time
+		pizza
+	end
+
+	def order_frequency
+		@frequency.count
+	end
+
+	def frequent_customer?
+		
+		@frequency.each do |time|
+			if Range.new(DateTime.now.to_date - 14, DateTime.now.to_date).include?(time.to_date)
+				@count += 1
+			end
+		end
+
+		if @count > 2 
+			true
+		end
+	end
+
+
+end
