4d1eed940eb289894b7933e2324d0ebcce0c1bed
Ken Lee <wewert@gmail.com>
2017-02-24 14:17:10 -0700

    here it goes

---
+++ lib/customer.rb
@@ -0,0 +1,27 @@
+class Customer
+  def initialize(name, address, phone)
+    counter = 0
+  end
+
+  def frequent_customer?
+    false
+  end
+
+  def order_in_place?
+    if place_order == true
+    end
+    counter += 1
+  end
+
+  def place_order(pizza, time)
+    true
+  end
+
+  def order_frequency
+    0
+  end
+
+  def customer_profile
+    profile == profile
+  end
+end
