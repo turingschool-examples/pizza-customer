b9eda03d757a016a840c9cf0913fb37745fa5ce1
Sam Lim <limsammy1@gmail.com>
2017-04-14 10:17:49 -0600

    Got calculate price and another price tests passing.

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -1,4 +1,7 @@
+require_relative 'prices'
+
 class Pizza
+  include Prices
   attr_reader :size, :type, :crust
 
   def initialize(size, type, crust)
@@ -13,5 +16,13 @@ class Pizza
       :type => type,
       :crust => crust
     }
+  end
+
+  def calculate_price
+    full_order.reduce(0) do |price, (key, val)|
+      val = "special" if key == :type && val != "cheese"
+      price += pizza_prices[key][val.to_sym]
+      price
+    end
   end
 end
