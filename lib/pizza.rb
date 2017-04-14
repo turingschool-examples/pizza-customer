d5ea965b7d04a20f37cd0c9f2997f377ad881454
Chris Smith <42@chrisryansmith.com>
2017-04-14 10:17:39 -0600

    get pizza initialized and setup order

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -1,4 +1,15 @@
 require 'pry'
 
 class Pizza
+  def initialize(*args)
+    @order = {
+      size: args[0],
+      type: args[1],
+      crust: args[2]
+    }
+  end
+
+  def full_order
+    @order
+  end
 end
