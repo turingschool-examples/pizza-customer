eaeb2d7197c6abf68a94a38b12616e11a0654195
marcoverzero <marcoverzero@gmail.com>
2017-04-14 10:26:21 -0600

    pizzas ordered and priced

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -1,5 +1,5 @@
 class Pizza
-  attr_reader
+  attr_reader :size, :type, :crust
     def initialize(size, type, crust)
       @size = size
       @type = type
@@ -8,12 +8,20 @@ class Pizza
 
     def full_order
       new_order = {}
-      new_order[:size] = @size
-      new_order[:type] = @type
-      new_order[:crust] = @crust
+      new_order[:size] = size
+      new_order[:type] = type
+      new_order[:crust] = crust
       new_order
     end
 
     def calculate_price
-      
+      price = 0.00
+      if size == "medium"
+        price += 12.50
+      elsif size == "large"
+        price += 15.00
+      else
+        pirce
+      end
+    end
 end
