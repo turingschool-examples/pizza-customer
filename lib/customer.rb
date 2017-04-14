a881701c965c63f9a95f79035dcf649a5cae9b13
marcoverzero <marcoverzero@gmail.com>
2017-04-14 11:16:35 -0600

    fun challenge with time

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,13 @@
+
 class Customer
-  attr_reader :name, :address, :phone, :order
+  attr_reader :name, :address, :phone, :ordered, :order_frequency
   def initialize(name, address, phone)
     @name = name
     @address = address
     @phone = phone
-    @order = nil
+    @ordered = false
+    @order_frequency = 0
+    @order_record = []
   end
 
   def profile
@@ -16,11 +19,25 @@ class Customer
   end
 
   def order_in_place?
-    order
+    ordered
   end
 
   def place_order(pizza, time)
-    time = DateTime.now
-    order = pizza
+    @order_frequency += 1
+    @order_record << time
+
+    @ordered = true
   end
+
+  def frequent_customer?
+    if @order_frequency > 2
+      @order_record.count(DateTime.now
+    else
+      false
+    end
+  end
+
+
+
+
 end
