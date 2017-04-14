26556ee7668841fb709765a504d04869220a9b54
alex549us3 <alex549us3@gmail.com>
2017-04-14 10:22:53 -0600

    first test passing + extras

---
+++ lib/pizza.rb
@@ -0,0 +1,19 @@
+class Pizza
+
+  def initialize(size, type, crust)
+    @size = size
+    @type = type
+    @crust = crust
+  end
+
+  def new_order(args)
+    size = args[:size]
+    type = args[:type]
+    crust = args[:crust]
+
+  end
+
+  def full_order
+  end
+
+end
