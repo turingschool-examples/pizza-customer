840695b2eb9b0e4c41b742440162bdfc62c9007f
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-14 11:57:24 -0600

    Commits calculate_price method with passing tests

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -2,7 +2,7 @@ require './lib/prices'
 
 class Pizza
   include Prices
-
+  attr_accessor :size, :type, :crust
   def initialize(size, type, crust)
     @size = size
     @type = type
@@ -14,6 +14,18 @@ class Pizza
   end
 
   def calculate_price
-    pizza_prices.values_at(@size, @type, @crust)
+    crust_price + size_price + type_price
+  end
+
+  def crust_price
+    pizza_prices[:crust][@crust.to_sym]
+  end
+
+  def size_price
+    pizza_prices[:size][@size.to_sym]
+  end
+
+  def type_price
+    pizza_prices[:type][@type.to_sym]
   end
 end
