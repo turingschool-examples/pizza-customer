c5d506fb73fdcdef93b909cc14568b971c7435b1
Katie Shermer <katieshermer@gmail.com>
2017-05-26 11:29:59 -0600

    Added calculate_frequent_customer method to Customer class; tests all pass now

--- lib/customer.rb
+++ lib/customer.rb
@@ -5,6 +5,7 @@ class Customer
   attr_reader :name,
               :address,
               :phone,
+              :orders,
               :order_in_place,
               :order_frequency
 
@@ -12,6 +13,7 @@ class Customer
     @name = name
     @address = address
     @phone = phone
+    @orders = []
     @order_in_place = false
     @order_frequency = 0
   end
@@ -28,10 +30,20 @@ class Customer
 
   def place_order(pizza, date_time)
     @order_in_place = true
+    @orders << [pizza, date_time]
     @order_frequency += 1
   end
 
+  def calculate_order_frequency
+    @orders.each do |order|
+      if (DateTime.now.to_date - order[1].to_date) > 30
+        @order_frequency -= 1
+      end
+    end
+  end
+
   def frequent_customer?
+    calculate_order_frequency
     true if @order_frequency > 2
   end
 
