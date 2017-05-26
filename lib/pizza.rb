c6611683d408af373bab888dc8d78b476a78683f
erose357 <erose357@gmail.com>
2017-05-26 11:20:05 -0600

    add calculate price

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -6,4 +6,14 @@ class Pizza
     @crust = crust
   end
 
+  def calculate_price
+    if @size == "large"
+      15.00
+    elsif @size == "medium"
+      12.50
+    else
+      0
+    end
+  end
+
 end
