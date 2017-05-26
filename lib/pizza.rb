018845614cca7d3fa44b993cfe6d1254865c772c
Ellen Cooper <ellencooper74@gmail.com>
2017-05-26 11:43:29 -0600

    all Pizza tests pass

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -1,11 +1,17 @@
 require "./lib/prices"
 
 class Pizza
+  include Prices
   attr_reader :size, :type, :crust
   def initialize(size, type, crust)
     @size = size
     @type = type
     @crust = crust 
+  end 
+  
+  def type_for_pricing
+    return "special" if @type != "cheese"
+    return "cheese"
   end 
   
   def full_order
@@ -15,8 +21,9 @@ class Pizza
   end 
   
   def calculate_price
-    
+    size_price = pizza_prices[:size][@size.intern]
+    type_price = pizza_prices[:type][type_for_pricing.intern]
+    crust_price = pizza_prices[:crust][@crust.intern]
+    size_price + type_price + crust_price
   end 
-  
-
 end 
