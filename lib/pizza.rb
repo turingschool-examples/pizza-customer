aba58b5666d9069f69975e37703febe347c28bce
wratterman <will.ratterman@gmail.com>
2017-05-26 11:29:35 -0600

    final test working, will add test if I have time

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -20,23 +20,39 @@ class Pizza
   end
 
   def calculate_price
-    identify_pizza(new_order)
+    identify_pizza(full_order)
   end
 
   def identify_pizza(order)
-    identify_size(order)
-    identify_type(order)
-    identify_crust(order)
+    cost = identify_size(order) + identify_type(order) + identify_crust(order)
+    cost
   end
 
   def identify_size(order)
+    if order[:size] == "small"
+      pizza_prices[:size][:small]
+    elsif order[:size] == "medium"
+      pizza_prices[:size][:medium]
+    elsif order[:size] == "large"
+      pizza_prices[:size][:large]
+    end
   end
 
   def identify_type(order)
+    if order[:type] == "cheese"
+      pizza_prices[:type][:cheese]
+    else
+      pizza_prices[:type][:special]
+    end
   end
 
   def identify_crust(order)
+    if order[:crust] == "thin"
+      pizza_prices[:crust][:thin]
+    elsif order[:crust] == "regular"
+      pizza_prices[:crust][:regular]
+    elsif order[:crust] == "deep dish"
+      pizza_prices[:crust][:deep_dish]
+    end
   end
-
-
 end
