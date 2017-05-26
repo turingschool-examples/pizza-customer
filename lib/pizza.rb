bce83209fdbb4f497ad2a2b6219cd361b5a46529
MikelSage <michael.sagapolutele@yahoo.com>
2017-05-26 11:32:43 -0600

    adds calculate_price to Pizza

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -1,4 +1,8 @@
+require_relative 'prices'
+require "pry"
+
 class Pizza
+  include Prices
 
   def initialize(size, type, crust)
     @size = size
@@ -15,6 +19,15 @@ class Pizza
   end
 
   def calculate_price
-    12.50
+    total_price = 0.0
+
+    pizza_prices.each do |trait, prices|
+      prices.each do |choice, price|
+        if full_order[trait].to_sym == choice
+          total_price += price
+        end
+      end
+    end
+    total_price
   end
 end
