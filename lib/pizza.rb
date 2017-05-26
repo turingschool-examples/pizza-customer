e440a8e58945a257db93b4b1199b6a3a2c0db4b5
BFunk <bret.e.funk@gmail.com>
2017-05-26 11:38:46 -0600

    add pizza file, class and methods

---
+++ lib/pizza.rb
@@ -0,0 +1,43 @@
+require './lib/prices'
+
+class Pizza
+  include Prices
+  attr_accessor :size, :type, :crust
+  def initialize (size, type, crust)
+    @size = size
+    @type = type
+    @crust = crust
+  end
+
+  def full_order
+    {:size=>size, :type=>type, :crust=>crust}
+  end
+
+  def calculate_price
+    price = 0.0
+    price += pizza_prices[:size][size.to_sym]
+    price += pizza_prices[:type][type.to_sym]
+    price += pizza_prices[:crust][crust.to_sym]
+    return price
+  end
+
+
+def pizza_prices
+  {
+    size: {
+      "small": 4.00,
+      "medium": 5.50,
+      "large": 7.00
+    },
+    type: {
+      "cheese": 3.00,
+      "special": 6.00
+    },
+    crust: {
+      "regular": 3.00,
+      "thin": 2.00,
+      "deep dish": 4.00
+    }
+  }
+end
+end
