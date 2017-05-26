f0e3bb7f9629dbe97218c7ca53a01f30e6ec64e2
wratterman <will.ratterman@gmail.com>
2017-05-26 11:35:01 -0600

    create profile method in Customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -3,4 +3,12 @@ class Customer
   def initialize(name, address, phone_number)
   end
 
+  def profile
+    profile = {
+      name: "Lauren",
+      address: "1700 Blake St.",
+      phone: "505-110-1001"
+    }
+  end
+
 end
