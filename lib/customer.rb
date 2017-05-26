2cc32a2f1d65b09b04d25e17f7636c2b6c5e95be
Katie Shermer <katieshermer@gmail.com>
2017-05-26 11:18:59 -0600

    Created and refined order_in_place? and place_order methods in Customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,14 @@
+require './lib/pizza.rb'
+
 class Customer
 
-  attr_reader :name, :address, :phone
+  attr_reader :name, :address, :phone, :order_in_place
 
   def initialize(name, address, phone)
     @name = name
     @address = address
     @phone = phone
+    @order_in_place = false
   end
 
   def profile
@@ -15,7 +18,11 @@ class Customer
   end
 
   def order_in_place?
-    false
+    @order_in_place
+  end
+
+  def place_order(pizza, date_time)
+    @order_in_place = true
   end
 
 end
